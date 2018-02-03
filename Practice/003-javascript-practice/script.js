5fe5c38bb7c1d43c571a9ac054de95bc7a73d9f4
devatha k <k.n.devs@gmail.com>
2018-02-02 19:33:11 -0500

    basic-objects

---
+++ Practice/003-javascript-practice/script.js
@@ -0,0 +1,100 @@
+console.log("Console.log says Hello");
+
+
+
+// var flowerName = "Lilly";
+// console.log(flowerName);
+
+
+
+// if (flowerName == "Lilly"){
+// 	console.log("This is lilly flower")
+// }
+// else
+// {
+// 	console.log("This is not lilly")
+// };
+
+
+
+// function addSomething(num1, num2){
+// 	console.log(num1 + num2)
+// }
+// addSomething(2,3)
+
+// function ifSomething(param){
+// 	if (param === "lilly"){
+// 		console.log("this is a flower")
+// 	}
+// 	else{
+// 		console.log("this is not a flower")
+// 	}
+// }
+
+// ifSomething("lilly")
+
+
+
+// function functionInside (param){
+// 	if (param === "lilly") {
+// 		function funcOne() {
+// 			console.log(param + "is a yellow flower")
+// 		}
+// 	}
+// 	else{
+// 		function FuncTwo(){
+// 			console.log(param + "is not a yellow flower")
+// 		}
+// 	}
+// }
+
+// var someArray = ["apple", "banana", "cherry"];
+
+
+// someArray.map(function(i, e) {
+//    console.log((i).replace("apple", "grapes"), e)
+// });
+
+
+
+
+// somestring.replace("apple")
+
+
+
+// using for: 
+// for (var i = 0; i < someArray.length; i++) {
+// 	console.log(someArray[i])
+// };
+
+
+// simple map example:
+// someArray.map(function(i, e) {
+//    console.log(i, e)
+// })
+
+
+// someArray.forEach(function(i, e){
+// 	console.log(i, e)
+// } )
+
+
+// var somename = {
+// 	firstName: ["Neya", "nimi"],
+// 	gender: "female",
+//   hobby: "tennis"
+// }
+
+// for(var prop in somename){
+// 	console.log(somename[prop]);
+// }
+
+
+
+
+
+
+
+
+
+
