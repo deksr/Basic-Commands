6a060d84c688812b457c859e3a9cb7034c411c3d
devatha k <k.n.devs@gmail.com>
2018-03-17 14:31:59 -0400

    added comments

--- learning-vue/vueproj/src/vueJS/vue-related-scripts.js
+++ learning-vue/vueproj/src/vueJS/vue-related-scripts.js
@@ -1,8 +1,9 @@
 console.log("vue-related-javascripts");
 
+
 // route components
 var Foo = { 
-	template: '<div>foo</div>' 
+	template: '<div class="template-1">foo</div>' ,
 }
 
 var Bar = { 
@@ -10,7 +11,7 @@ var Bar = {
 }
 
 var Shop = { 
-	template: '<div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>' 
+	template: '<div>lorems</div>' 
 }
 
 
@@ -42,5 +43,9 @@ var router = new VueRouter({
 var app = new Vue({
   router
 }).$mount('#app-render-one')
+
+
+
+
 
 
